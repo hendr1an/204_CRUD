f04696afddb8c340306de78cff6c9d10a9ccaa48
hendr1an <hendriansyahgyan@gmail.com>
2025-10-21 10:11:05 +0700

    Tambah endpoint GET /api/Mahasiswa

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+let mysql = require('mysql');
+const app = express();
+const port = 3000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'gyan1234',
+  database: 'biodata',
+  port : 3307
+});
+
+db.connect((err) => {
+    if (err) {
+        console.error('Error connecting to MySQL: ' + err.stack);
+        return;
+    }
+    console.log('Connection Successfully');
+});
+
+app.get('/api/Mahasiswa', (req, res) => {
+    db.query('SELECT * FORM mahasiswa', (err, results) => {
+       if (err) {
+            console.error('Error executing query : ' + err.stack) 
+    })
+})
