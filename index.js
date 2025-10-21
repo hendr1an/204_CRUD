ec23e40fe2b312d1ccb5c44ea5ea29e81c90eef4
hendr1an <hendriansyahgyan@gmail.com>
2025-10-21 10:23:03 +0700

    Tambah endpoint POST untuk simpan data mahasiswa baru

--- index.js
+++ index.js
@@ -39,3 +39,15 @@ app.get('/api/Mahasiswa', (req, res) => {
     res.json(results);
     });
 });
+
+app.post('/api/mahasiswa', (req, res) =>{
+    const { nama, nim, kelas, prodi } = req.body;
+
+    if (!nama || !nim || !kelas || !prodi) {
+        return res,status(400).json({ message: 'nama, nim, kelas, prodi wajib di isi'});
+    }
+
+    db.query(
+        'INSERT INTO mahasiswa (nama, nim, kelas, prodi) VALUES (?, ?, ?, ?)',
+    )
+})
