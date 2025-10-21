70bd81664637ed9283cd914dd42cd74660f470b1
hendr1an <hendriansyahgyan@gmail.com>
2025-10-21 10:42:44 +0700

    perubahan port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 let mysql = require('mysql');
 const app = express();
-const port = 3000;
+const port = 3307;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
